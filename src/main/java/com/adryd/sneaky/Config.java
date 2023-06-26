d06a9ae894c8ff9116e8c5278a57118b97ccac93
adryd <me@adryd.com>
2023-06-26 11:47:05 -0400

    Update Config.java

--- src/main/java/com/adryd/sneaky/Config.java
+++ src/main/java/com/adryd/sneaky/Config.java
@@ -62,14 +62,14 @@ public class Config {
 
         hideServerPingData = asBoolean((String) properties.computeIfAbsent("hide-server-ping-data", (a) -> "true"), true);
         onlyHidePlayerList = asBoolean((String) properties.computeIfAbsent("hide-player-list", (a) -> "false"), false);
-        dontLogClientDisconnects = asBoolean((String) properties.computeIfAbsent("dont-log-unauthed-client-disconnects", (a) -> "false"), false);
+        dontLogClientDisconnects = asBoolean((String) properties.computeIfAbsent("dont-log-unauthed-client-disconnects", (a) -> "true"), true);
         dontLogServerDisconnects = asBoolean((String) properties.computeIfAbsent("dont-log-unauthed-server-disconnects", (a) -> "false"), false);
         rateLimitNewConnections = asBoolean((String) properties.computeIfAbsent("rate-limit-new-connections", (a) -> "true"), true);
         newConnectionRateLimit = asInteger((String) properties.computeIfAbsent("new-connection-rate-limit", (a) -> "5"), 5);
 
         try (FileOutputStream stream = new FileOutputStream(FILE)) {
             properties.store(stream, "Sneaky Server properties file\n" +
-                    "Read https://modrinth.com/mod/sneaky-server for more information");
+                    "Please read https://modrinth.com/mod/sneaky-server for more information, Not every config option is straight forward");
         } catch (IOException e) {
             Sneaky.LOGGER.warn("[\" + Sneaky.MOD_ID + \"] Could not write config '" + FILE.getAbsolutePath() + "'", e);
         }
