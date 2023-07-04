88255b0ebb7ae4fa2344bdfd72b89cec658c2a51
adryd <me@adryd.com>
2023-07-04 17:06:28 -0400

    Update Config.java
    default dontLogClientDisconnects to false since the cuute login spam seems to have stopped

--- src/main/java/com/adryd/sneaky/Config.java
+++ src/main/java/com/adryd/sneaky/Config.java
@@ -59,7 +59,7 @@ public class Config {
 
         hideServerPingData = asBoolean((String) properties.computeIfAbsent("hide-server-ping-data", (a) -> "true"), true);
         onlyHidePlayerList = asBoolean((String) properties.computeIfAbsent("hide-player-list", (a) -> "false"), false);
-        dontLogClientDisconnects = asBoolean((String) properties.computeIfAbsent("dont-log-unauthed-client-disconnects", (a) -> "true"), true);
+        dontLogClientDisconnects = asBoolean((String) properties.computeIfAbsent("dont-log-unauthed-client-disconnects", (a) -> "false"), false);
         dontLogServerDisconnects = asBoolean((String) properties.computeIfAbsent("dont-log-unauthed-server-disconnects", (a) -> "false"), false);
         rateLimitNewConnections = asBoolean((String) properties.computeIfAbsent("rate-limit-new-connections", (a) -> "true"), true);
         newConnectionRateLimit = asInteger((String) properties.computeIfAbsent("new-connection-rate-limit", (a) -> "5"), 5);
